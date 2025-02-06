a72e2c0f241d048c0f6ad50465e856d4821177b6
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-02-06 23:57:06 +0530

    styling for weather app

---
+++ src/Weather.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import "./Weather.css";
+
+const Weather = () => {
+  let apiKey = "127e814c921d448ebb7164527252301";
+  const [location, setLocation] = useState("Wakad");
+  const [weatherData, setWeatherData] = useState({
+    location: "",
+    region: "",
+    country: "",
+    temp: "",
+    faranhite: "",
+  });
+
+  useEffect(() => {
+    fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=7&aqi=no&alerts=no`
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response.current);
+        setWeatherData({
+          location: response.location.name,
+          region: response.location.region,
+          country: response.location.country,
+          temp: response.current.temp_c,
+          faranhite: response.current.temp_f,
+        });
+      });
+  }, []);
+  return (
+    <div className="weather">
+      <div className="head">
+        <h1>
+          <i className="fa-solid fa-location-dot"></i>
+        </h1>
+        <h2>{weatherData.location}, </h2>
+        <h4>{weatherData.region}</h4>
+        <h4>{weatherData.country}</h4>
+      </div>
+
+      <div className="temp">
+        <h1>{weatherData.temp}°C</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
