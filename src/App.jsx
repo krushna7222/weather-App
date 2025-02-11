fdad9f79185c71b24f321dea5969e6a29b2f0ad6
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-02-11 19:54:42 +0530

    App.js is  updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
-import Weather from "./Weather";
+import Env from "./Env";
+import Temp from "./Temp";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <>
       <Weather></Weather>
+      {/* <Temp></Temp> */}
+      {/* <Env></Env> */}
     </>
   );
 }
